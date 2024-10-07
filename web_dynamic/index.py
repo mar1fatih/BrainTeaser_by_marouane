036d0a4a453bcbbc79d5211b33b7282b9039aae8
mar1fatih <marouanefatih631@gmail.com>
2024-10-07 15:13:31 +0100

    adding templates

--- web_dynamic/index.py
+++ web_dynamic/index.py
@@ -3,3 +3,14 @@
 from flask import Flask, render_template, abort
 from datetime import datetime
 import requests
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ main page """
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5003')
